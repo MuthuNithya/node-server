c3f716c7db33b700f7224ffb3fab2a281b08e8df
MuthuNithya <muthukrishnan.suresh1987@gmail.com>
2015-09-16 10:56:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -23,9 +23,9 @@
     });
     var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
     var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-    var server = require('http').createServer(app);
+    //var server = require('http').createServer(app);
     console.log(" on " + server_ip_address + ", server_port " + server_port)
-    server.listen(server_port, server_ip_address, function(){
+    app.listen(server_port, server_ip_address, function(){
       console.log("Listening on " + server_ip_address + ", server_port " + server_port)
     });
 })();
