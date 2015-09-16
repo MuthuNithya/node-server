adc48620760a982a41fecad205c8124ca76eb675
MuthuNithya <muthukrishnan.suresh1987@gmail.com>
2015-09-16 10:35:57 -0700

    added cors

--- server.js
+++ server.js
@@ -3,17 +3,18 @@
     var app = express();
     var repoConnect = require('./MongoDB.js');
     var bodyParser = require('body-parser');
-    var WebSocketServer = require('ws').Server;
+    var cors = require('cors');
     // Create application/x-www-form-urlencoded parser
     app.use(bodyParser.json());       // to support JSON-encoded bodies
+    app.use(cors());
     app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
         extended: true
     }));
-    app.use(function(req, res, next) {
+    /*app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept");
         next();
-    });
+    });*/
     app.post('/auth_login', function (req, res) {
         repoConnect.authorizeLogin('login', req.body, res);
     });
@@ -23,17 +24,6 @@
     var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
     var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
     var server = require('http').createServer(app);
-    wss = new WebSocketServer({
-        server: server,
-        autoAcceptConnections: false
-    });
-    wss.on('connection', function(ws) {
-      console.log("New connection");
-      ws.on('message', function(message) {
-        ws.send("Received: " + message);
-      });
-      ws.send('Welcome!');
-    });
     console.log(" on " + server_ip_address + ", server_port " + server_port)
     server.listen(server_port, server_ip_address, function(){
       console.log("Listening on " + server_ip_address + ", server_port " + server_port)
